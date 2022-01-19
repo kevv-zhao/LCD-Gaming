564cf4215fbb3d22e2326f1ae4ed505a89b24c49
kevv_zhao <zhao.kevin@huskers.unl.edu>
2022-01-19 16:25:02 -0600

    Added the necessary libraries for TFT LCD from Adafruit and initialized and set up the display.

--- pong_and_sketch/src/pong_and_sketch.cpp
+++ pong_and_sketch/src/pong_and_sketch.cpp
@@ -1,7 +1,36 @@
-#include <Arduino.h>
+/*--------------------------------------------------------------------
+Name:   Kevin Zhao
+Date:   2022, January 19
+File:   pong_and_sketch.cpp
+
+Purp: Demonstrates the use of Adafruit 1.8" (128x160 pixels) TFT Display 
+      using the Adafruit GFX and ST7735 libraries to be used to make an
+      Etch-a-Sketch and Pong Game. 
+
+Doc:  Used seesaw_shield18_test.ino to get initialization values, 
+      and uses the Adafruit GFX, Adafruit ST7735, and Adafruit
+      BusIO Libraries, Prof. Falkinburg
+--------------------------------------------------------------------*/
+
+#include <Arduino.h>         // jlf added for vs_code
+#include <Wire.h>            // jlf added for vs_code
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include <SPI.h>
+
+// These pins can be changed
+#define TFT_CS    10
+#define TFT_RST   9 
+#define TFT_DC    8
+
+// Create class named tft of type Adafruit_ST7735
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
 void setup() {
-  // put your setup code here, to run once:
+  // Use this initializer if using a 1.8" TFT screen:
+  tft.initR(INITR_GREENTAB);      // Init ST7735S chip, green tab
+
+  tft.fillScreen(ST77XX_BLACK);
 }
 
 void loop() {
