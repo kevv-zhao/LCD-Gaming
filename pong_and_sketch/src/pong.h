c5d9f236c94ce364386484af25922201284093cd
kevv_zhao <zhao.kevin@huskers.unl.edu>
2022-01-21 23:41:48 -0600

    Stored paddle dimensions in variables.

--- pong_and_sketch/src/pong.h
+++ pong_and_sketch/src/pong.h
@@ -10,31 +10,33 @@ Doc:
 
 int16_t paddlePos[2];
 int8_t paddleSpeed = 20;
+int8_t paddleWidth = 4;
+int8_t paddleLength = 24;
 
 void paddleInit(Adafruit_ST7735 tft) {
     // Paddle Setup
-    paddlePos[0] = tft.height()/2 - 12; paddlePos[1] = tft.height()/2 - 12;
-    tft.fillRect(4, paddlePos[0], 4, 24, ST7735_WHITE);
+    paddlePos[0] = tft.height()/2 - paddleLength/2; paddlePos[1] = tft.height()/2 - paddleLength/2;
+    tft.fillRect(4, paddlePos[0], paddleWidth, paddleLength, ST7735_WHITE);
 }
 
 void paddleMove(Adafruit_ST7735 tft, int xVal, int yVal) {
     // Paddle movement
     if((paddlePos[0] + (yVal*paddleSpeed/511) < 0 && yVal < -20) || 
-    (paddlePos[0] + (yVal*paddleSpeed/511) > tft.height()-24 && yVal > 20)) {
+    (paddlePos[0] + (yVal*paddleSpeed/511) > tft.height()-paddleLength && yVal > 20)) {
         // Edge cases for when the paddle nears the edge of the screen and is stopped
-        tft.fillRect(4, paddlePos[0], 4, 24, ST7735_BLACK);
+        tft.fillRect(4, paddlePos[0], paddleWidth, paddleLength, ST7735_BLACK);
         if(yVal*paddleSpeed/511 < 0) {
-        paddlePos[0] = 0;
+            paddlePos[0] = 0;
         } else {
-        paddlePos[0] = tft.height()-24;
+            paddlePos[0] = tft.height()-paddleLength;
         }
-        tft.fillRect(4, paddlePos[0], 4, 24, ST7735_WHITE);
+        tft.fillRect(4, paddlePos[0], paddleWidth, paddleLength, ST7735_WHITE);
 
-    } else if((paddlePos[0] >= 0 && yVal < -20) || (paddlePos[0] <= tft.height()-24 && yVal > 20)) {
+    } else if((paddlePos[0] >= 0 && yVal < -20) || (paddlePos[0] <= tft.height()-paddleLength && yVal > 20)) {
         // General paddle movement, non-edge cases
-        tft.fillRect(4, paddlePos[0], 4, 24, ST7735_BLACK);
+        tft.fillRect(4, paddlePos[0], paddleWidth, paddleLength, ST7735_BLACK);
         paddlePos[0] = paddlePos[0] + (yVal*paddleSpeed/511);
-        tft.fillRect(4, paddlePos[0], 4, 24, ST7735_WHITE);
+        tft.fillRect(4, paddlePos[0], paddleWidth, paddleLength, ST7735_WHITE);
     }
 }
 
